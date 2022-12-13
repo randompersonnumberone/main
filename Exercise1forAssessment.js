f90ad005a718e97406a8680ee06dc1eb243e7e2f
randompersonnumberone <120495906+randompersonnumberone@users.noreply.github.com>
2022-12-13 16:48:07 +0000

    Add files via upload

---
+++ Exercise1forAssessment.js
@@ -0,0 +1,12 @@
+// Exercise1forAssessment.js
+// Shirley Chang - 2022 - shirley.chang.19@ucl.ac.uk
+// Last changed 21/10/2022
+
+/* This program prints Hello World.             */
+
+function main ()
+{
+
+  alert ('Hello World!');
+
+}
